c541ba36a11794e6bbb94d461f0783e20e094afa
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-15 15:43:45 +0530

    Added : Email Validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -18,3 +18,12 @@ else
         echo "Invalid Last Name"
 fi
 
+emailPattern="^[A-Za-z0-9]+([._+-][a-zA-Z0-9]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][A-Za-z]{2})$"
+read -p " Enter Your Email ID : " email
+if [[ $email =~ $emailPattern ]]
+then
+        echo "Valid Email ID"
+else
+        echo "Invalid Email ID"
+fi
+
